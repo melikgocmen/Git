531c3af5b48aba132ddcf27164c453fd4bf78828
hejiaxing <360104250@qq.com>
2018-09-27 16:50:38 +0200

    update to phase1

---
+++ src/swsv-ris/src/test/java/hu/bme/mit/swsv/ris/tsm/test/SafetyLogicDistributedTest.java
@@ -0,0 +1,5 @@
+package hu.bme.mit.swsv.ris.tsm.test;
+
+public class SafetyLogicDistributedTest {
+	// TODO Implement the distributed decision logic with TDD.
+}
---
+++ src/swsv-ris/src/test/java/hu/bme/mit/swsv/ris/tsm/test/SafetyLogicLocalTest.java
@@ -0,0 +1,5 @@
+package hu.bme.mit.swsv.ris.tsm.test;
+
+public class SafetyLogicLocalTest {
+	// TODO Create tests for the local decision logic.
+}
